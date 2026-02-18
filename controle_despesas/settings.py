d1cb29c97a9c86407e391c3ab01e4b32d9280824
Rogerio Campos <rogerio.campos07@gmail.com>
2026-02-18 15:07:58 -0300

    Remover linha extra conforme linter

--- controle_despesas/settings.py
+++ controle_despesas/settings.py
@@ -2,6 +2,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file='.env')
     DATABASE_URL: str
